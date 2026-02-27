f947bf470aaa6145395088c1b5a989ee9235a710
Dmytrii Penkivskiy <dmitrijpenkivskij@gmail.com>
2026-02-27 18:53:16 +0000

    fix: improve scalability and fix infinite loop while generating lastNickname

--- utils/nicknameGen.js
+++ utils/nicknameGen.js
@@ -3,8 +3,8 @@ const nouns = ["Lion", "Eagle", "Shark", "Wolf", "Panther", "Falcon", "Tiger", "
 export function* generateNickname() {
     while (true) {
         const id = Math.floor(Math.random() * 9999);
-        const adjective = adjectives[Math.floor(Math.random() * 7)];
-        const noun = nouns[Math.floor(Math.random() * 7)];
+        const adjective = adjectives[Math.floor(Math.random() * (adjectives.length))];
+        const noun = nouns[Math.floor(Math.random() * (nouns.length))];
         const nickname = adjective + noun + "#" + id;
         yield nickname;
     }
