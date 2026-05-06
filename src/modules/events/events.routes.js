1a12c310aa746d15130e66d1ef5c815f33bd583e
ApostolOleg <apostololeg0@gmail.com>
2026-05-06 13:33:06 +0300

    feat: add server-sent events (SSE) functionality with event routes

---
+++ src/modules/events/events.routes.js
@@ -0,0 +1,43 @@
+import { EventEmitter, on } from "node:events";
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
+export const EventSSE = new EventEmitter();
+EventSSE.setMaxListeners(0);
+
+async function* eventsGenerator() {
+	const sseEventIter = on(EventSSE, "SSE_EVENT");
+
+	for await (const [event] of sseEventIter) {
+		yield event;
+	}
+}
+
+setInterval(() => emitCreateQuizSSE(10), 500);
+setInterval(() => emitUpdateQuizSSE(20), 2500);
+
+export function emitCreateQuizSSE(quizId) {
+	EventSSE.emit("SSE_EVENT", `event: CREATE_QUIZ\ndata: ${quizId}\n\n`);
+}
+
+export function emitUpdateQuizSSE(quizId) {
+	EventSSE.emit("SSE_EVENT", `event: UPDATE_QUIZ\ndata: ${quizId}\n\n`);
+}
+
+export async function eventRoutes(fastify) {
+	fastify.get("/events", async (request, reply) => {
+		reply.raw.writeHead(200, {
+			"Content-Type": "text/event-stream",
+			"Cache-Control": "no-cache",
+			Connection: "keep-alive",
+		});
+
+		try {
+			await pipeline(Readable.from(eventsGenerator()), reply.raw);
+		} catch (err) {
+			if (err.code !== "ERR_STREAM_PREMATURE_CLOSE") {
+				request.log.error(err);
+			}
+		}
+	});
+}
