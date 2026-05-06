0fe8efef8836b19dbf059cf6bc8fefa103a5266f
ApostolOleg <apostololeg0@gmail.com>
2026-05-06 13:33:06 +0300

    feat: add server-sent events (SSE) functionality with event routes

--- src/app/http/router.js
+++ src/app/http/router.js
@@ -2,10 +2,13 @@ import { authRoutes } from "#src/modules/auth/index.js";
 import { quizRoutes } from "#src/modules/quizzes/index.js";
 import { resultRoutes } from "#src/modules/results/index.js";
 import { userRoutes } from "#src/modules/users/index.js";
+import { eventRoutes } from "#src/modules/events/events.routes.js";
 
 export default async function router(app) {
 	app.register(authRoutes, { prefix: "/auth" });
 	app.register(quizRoutes, { prefix: "/api/quizzes" });
 	app.register(resultRoutes, { prefix: "/api/results" });
 	app.register(userRoutes, { prefix: "/api/user" });
+
+	app.register(eventRoutes, { prefix: "/api" });
 }
