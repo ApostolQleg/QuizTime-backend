f7a86c9945f061e7ee68bab7e65c3c6ce8a47d5f
ApostolOleg <apostololeg0@gmail.com>
2026-05-09 15:41:14 +0300

    feat: enhance eventController to support abort signal for SSE event generation

--- src/modules/events/events.controller.js
+++ src/modules/events/events.controller.js
@@ -1,11 +1,16 @@
 import { Readable } from "node:stream";
-
 import { eventsGenerator } from "#src/modules/events/services/eventGenerator.js";
 
-export const eventController = async (_, reply) => {
+export const eventController = async (request, reply) => {
 	reply.type("text/event-stream");
 	reply.header("Cache-Control", "no-cache");
 	reply.header("Connection", "keep-alive");
 
-	return reply.send(Readable.from(eventsGenerator()));
+	const ac = new AbortController();
+
+	request.raw.on("close", () => {
+		ac.abort();
+	});
+
+	return reply.send(Readable.from(eventsGenerator({ signal: ac.signal })));
 };
--- src/modules/events/services/eventGenerator.js
+++ src/modules/events/services/eventGenerator.js
@@ -1,10 +1,17 @@
 import { on } from "node:events";
 import { EventSSE } from "#src/modules/events/index.js";
 
-export async function* eventsGenerator() {
-	const sseEventIter = on(EventSSE, "SSE_EVENT", { signal });
+export async function* eventsGenerator({ signal } = {}) {
+	try {
+		const sseEventIter = on(EventSSE, "SSE_EVENT", { signal });
 
-	for await (const [event] of sseEventIter) {
-		yield event;
+		for await (const [event] of sseEventIter) {
+			yield event;
+		}
+	} catch (error) {
+		if (error.name === "AbortError") {
+			return;
+		}
+		throw error;
 	}
 }
