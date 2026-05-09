1d5ed556c6d89d4db4aa7a68ba28f1f517107355
ApostolOleg <apostololeg0@gmail.com>
2026-05-09 15:02:34 +0300

    fix: add signal option to SSE event iterator for improved event handling

--- src/modules/events/services/eventGenerator.js
+++ src/modules/events/services/eventGenerator.js
@@ -2,7 +2,7 @@ import { on } from "node:events";
 import { EventSSE } from "#src/modules/events/index.js";
 
 export async function* eventsGenerator() {
-	const sseEventIter = on(EventSSE, "SSE_EVENT");
+	const sseEventIter = on(EventSSE, "SSE_EVENT", { signal });
 
 	for await (const [event] of sseEventIter) {
 		yield event;
