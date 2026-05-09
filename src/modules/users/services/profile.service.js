02e4652fe9e65908e8c99e1f597b4607a479e946
dimpennn <dmitrijpenkivskij@gmail.com>
2026-05-09 15:26:21 +0300

    feat: add user statistics retrieval to current user profile

--- src/modules/users/services/profile.service.js
+++ src/modules/users/services/profile.service.js
@@ -1,5 +1,6 @@
 import { InvalidUserPayloadError, UserNotFoundError } from "../errors/user.error.js";
 import * as userRepository from "../repositories/user.repository.js";
+import UserStats from "#src/modules/stats/userStats.model.js";
 
 const sanitizeUser = (user) => {
 	const { passwordHash, ...userData } = user.toObject();
@@ -12,6 +13,8 @@ export const getCurrentUserProfile = async ({ userId }) => {
 		throw new UserNotFoundError();
 	}
 
+	const stats = await UserStats.findOne({ userId }).lean();
+
 	return {
 		user: {
 			_id: user._id,
@@ -21,6 +24,7 @@ export const getCurrentUserProfile = async ({ userId }) => {
 			themeColor: user.themeColor,
 			avatarType: user.avatarType,
 			googleId: user.googleId,
+			stats: stats || { quizzesPassedCount: 0 },
 		},
 	};
 };
