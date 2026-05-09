7265198c9406e3b82aca2ed88957a8fbf3dbd857
ApostolOleg <apostololeg0@gmail.com>
2026-05-09 13:26:37 +0300

    feat: update SSE emission to include full quiz object on creation and update

--- src/modules/events/services/observer.js
+++ src/modules/events/services/observer.js
@@ -9,12 +9,12 @@ export function emitPingSSE() {
 	EventSSE.emit("SSE_EVENT", "event: PING\n\n");
 }
 
-export function emitCreateQuizSSE(quizId) {
-	EventSSE.emit("SSE_EVENT", `event: CREATE_QUIZ\ndata: ${quizId}\n\n`);
+export function emitCreateQuizSSE(quiz) {
+	EventSSE.emit("SSE_EVENT", `event: CREATE_QUIZ\ndata: ${JSON.stringify(quiz)}\n\n`);
 }
 
-export function emitUpdateQuizSSE(quizId) {
-	EventSSE.emit("SSE_EVENT", `event: UPDATE_QUIZ\ndata: ${quizId}\n\n`);
+export function emitUpdateQuizSSE(quiz) {
+	EventSSE.emit("SSE_EVENT", `event: UPDATE_QUIZ\ndata: ${JSON.stringify(quiz)}\n\n`);
 }
 
 export function emitDeleteQuizSSE(quizId) {
