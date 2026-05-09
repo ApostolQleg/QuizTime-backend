36e1bf124f25fbf70cb007bffcc824b9b5ed71b2
dimpennn <dmitrijpenkivskij@gmail.com>
2026-05-09 14:49:37 +0300

    fix: update userId to ObjectId, remove versionKey

--- src/modules/stats/userStats.model.js
+++ src/modules/stats/userStats.model.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
-const userStatsSchema = new mongoose.Schema({
-	userId: {
-		type: String,
-		required: true,
-		unique: true
+const userStatsSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			unique: true,
+			ref: "User"
+		},
+		quizzesPassedCount: {
+			type: Number,
+			default: 0
+		},
 	},
-	quizzesPassedCount: {
-		type: Number,
-		default: 0
+	{
+		versionKey: false
 	},
-});
+);
 
 export const UserStats = mongoose.model("UserStats", userStatsSchema);
